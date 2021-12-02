dd88edce9aea4d8b7a9efd9057aec2c1ec166a9d
axr5239 <axr5239@rit.edu>
2021-12-02 17:36:44 -0500

    Testing

--- client/client.js
+++ client/client.js
@@ -13,6 +13,7 @@ const sendAjax = (action, data) => {
             window.location = result.redirect;
         },
         error: (xhr, status, error) => {
+            console.log("There is something wrong...");
             const messageObj = JSON.parse(xhr.responseText);
 
             handleError(messageObj.error);
