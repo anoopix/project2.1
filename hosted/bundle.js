38c0438abe75efab573ac4c83c8e89c7365102e7
axr5239 <axr5239@rit.edu>
2021-12-03 16:53:46 -0500

    Reset to Part-C

--- hosted/bundle.js
+++ hosted/bundle.js
@@ -1,25 +1,63 @@
 "use strict";
-"use strict";
 
 var handleError = function handleError(message) {
   $("#errorMessage").text(message);
 };
 
-var redirect = function redirect(response) {
-  window.location = response.redirect;
-};
-
-var sendAjax = function sendAjax(type, action, data, success) {
+var sendAjax = function sendAjax(action, data) {
   $.ajax({
     cache: false,
-    type: type,
+    type: "POST",
     url: action,
     data: data,
     dataType: "json",
-    success: success,
+    success: function success(result, status, xhr) {
+      window.location = result.redirect;
+    },
     error: function error(xhr, status, _error) {
       var messageObj = JSON.parse(xhr.responseText);
       handleError(messageObj.error);
     }
   });
 };
+
+$(document).ready(function () {
+  $("#signupForm").on("submit", function (e) {
+    e.preventDefault();
+
+    if ($("#user").val() == '' || $("#pass").val() == '' || $("#pass2").val() == '') {
+      handleError("All the fields need to be filled!");
+      return false;
+    }
+
+    if ($("#pass").val() !== $("#pass2").val()) {
+      handleError("The passwords need to match!");
+      return false;
+    }
+
+    sendAjax($("#signupForm").attr("action"), $("#signupForm").serialize());
+    return false;
+  });
+  $("#loginForm").on("submit", function (e) {
+    e.preventDefault();
+
+    if ($("#user").val() == '' || $("#pass").val() == '') {
+      handleError("A username and password must be entered!");
+      return false;
+    }
+
+    sendAjax($("#loginForm").attr("action"), $("#loginForm").serialize());
+    return false;
+  });
+  $("#makerForm").on("submit", function (e) {
+    e.preventDefault();
+
+    if ($("#maker").val() == '') {
+      handleError("A message needs to be typed before it can be chirped");
+      return false;
+    }
+
+    sendAjax($("#makerForm").attr("action"), $("#makerForm").serialize());
+    return false;
+  });
+});
