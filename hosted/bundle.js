baf5d2deb3a56ea78cdc6f9f2a30d68bd781ce5a
axr5239 <axr5239@rit.edu>
2021-12-03 15:37:09 -0500

    Trying to add React

--- hosted/bundle.js
+++ hosted/bundle.js
@@ -1,63 +1,25 @@
 "use strict";
+"use strict";
 
 var handleError = function handleError(message) {
   $("#errorMessage").text(message);
 };
 
-var sendAjax = function sendAjax(action, data) {
+var redirect = function redirect(response) {
+  window.location = response.redirect;
+};
+
+var sendAjax = function sendAjax(type, action, data, success) {
   $.ajax({
     cache: false,
-    type: "POST",
+    type: type,
     url: action,
     data: data,
     dataType: "json",
-    success: function success(result, status, xhr) {
-      window.location = result.redirect;
-    },
+    success: success,
     error: function error(xhr, status, _error) {
       var messageObj = JSON.parse(xhr.responseText);
       handleError(messageObj.error);
     }
   });
 };
-
-$(document).ready(function () {
-  $("#signupForm").on("submit", function (e) {
-    e.preventDefault();
-
-    if ($("#user").val() == '' || $("#pass").val() == '' || $("#pass2").val() == '') {
-      handleError("All the fields need to be filled!");
-      return false;
-    }
-
-    if ($("#pass").val() !== $("#pass2").val()) {
-      handleError("The passwords need to match!");
-      return false;
-    }
-
-    sendAjax($("#signupForm").attr("action"), $("#signupForm").serialize());
-    return false;
-  });
-  $("#loginForm").on("submit", function (e) {
-    e.preventDefault();
-
-    if ($("#user").val() == '' || $("#pass").val() == '') {
-      handleError("A username and password must be entered!");
-      return false;
-    }
-
-    sendAjax($("#loginForm").attr("action"), $("#loginForm").serialize());
-    return false;
-  });
-  $("#makerForm").on("submit", function (e) {
-    e.preventDefault();
-
-    if ($("#maker").val() == '') {
-      handleError("A message needs to be typed before it can be chirped");
-      return false;
-    }
-
-    sendAjax($("#makerForm").attr("action"), $("#makerForm").serialize());
-    return false;
-  });
-});
--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -3,88 +3,95 @@ const models = require('../models');
 const { Account } = models;
 
 const loginPage = (req, res) => {
-  res.render('login', { csrfToken: req.csrfToken() });
-};
-
-const signupPage = (req, res) => {
-  res.render('signup', { csrfToken: req.csrfToken() });
+    res.render('login', { csrfToken: req.csrfToken() });
 };
 
 const logout = (req, res) => {
-  req.session.destroy();
-  res.redirect('/');
+    req.session.destroy();
+    res.redirect('/');
 };
 
 const login = (request, response) => {
-  const req = request;
-  const res = response;
+    const req = request;
+    const res = response;
 
-  // Force cast to strings to cover up some security flaws
-  const username = `${req.body.username}`;
-  const password = `${req.body.pass}`;
+    // Force cast to strings to cover up some security flaws
+    const username = `${req.body.username}`;
+    const password = `${req.body.pass}`;
 
-  if (!username || !password) {
-    return res.status(400).json({ error: 'All the fields need to be filled!' });
-  }
-
-  return Account.AccountModel.authenticate(username, password, (err, account) => {
-    if (err || !account) {
-      return res.status(401).json({ error: 'Wrong username or password' });
+    if (!username || !password) {
+        return res.status(400).json({ error: 'All the fields need to be filled!' });
     }
 
-    req.session.account = Account.AccountModel.toAPI(account);
+    return Account.AccountModel.authenticate(username, password, (err, account) => {
+        if (err || !account) {
+            return res.status(401).json({ error: 'Wrong username or password' });
+        }
 
-    return res.json({ redirect: '/maker' });
-  });
+        req.session.account = Account.AccountModel.toAPI(account);
+
+        return res.json({ redirect: '/maker' });
+    });
 };
 
 const signup = (request, response) => {
-  const req = request;
-  const res = response;
+    const req = request;
+    const res = response;
 
-  // Cast to strings to cover up some security flaws
-  req.body.username = `${req.body.username}`;
-  req.body.pass = `${req.body.pass}`;
-  req.body.pass2 = `${req.body.pass2}`;
+    // Cast to strings to cover up some security flaws
+    req.body.username = `${req.body.username}`;
+    req.body.pass = `${req.body.pass}`;
+    req.body.pass2 = `${req.body.pass2}`;
 
-  if (!req.body.username || !req.body.pass || !req.body.pass2) {
-    return res.status(400).json({ error: 'All the fields need to be filled!' });
-  }
+    if (!req.body.username || !req.body.pass || !req.body.pass2) {
+        return res.status(400).json({ error: 'All the fields need to be filled!' });
+    }
 
-  if (req.body.pass !== req.body.pass2) {
-    return res.status(400).json({ error: 'The passwords need to match!' });
-  }
+    if (req.body.pass !== req.body.pass2) {
+        return res.status(400).json({ error: 'The passwords need to match!' });
+    }
 
-  return Account.AccountModel.generateHash(req.body.pass, (salt, hash) => {
-    const accountData = {
-      username: req.body.username,
-      salt,
-      password: hash,
+    return Account.AccountModel.generateHash(req.body.pass, (salt, hash) => {
+        const accountData = {
+            username: req.body.username,
+            salt,
+            password: hash,
+        };
+
+        const newAccount = new Account.AccountModel(accountData);
+
+        const savePromise = newAccount.save();
+
+        savePromise.then(() => {
+            req.session.account = Account.AccountModel.toAPI(newAccount);
+            return res.json({ redirect: '/maker' });
+        });
+
+        savePromise.catch((err) => {
+            console.log(err);
+
+            if (err.code === 11000) {
+                return res.status(400).json({ error: 'Username is already in use.' });
+            }
+
+            return res.status(400).json({ error: 'An error occurred' });
+        });
+    });
+};
+
+const getToken = (request, response) => {
+    const req = request;
+    const res = response;
+
+    const csrfJSON = {
+        csrfToken: req.csrfToken(),
     };
 
-    const newAccount = new Account.AccountModel(accountData);
-
-    const savePromise = newAccount.save();
-
-    savePromise.then(() => {
-      req.session.account = Account.AccountModel.toAPI(newAccount);
-      return res.json({ redirect: '/maker' });
-    });
-
-    savePromise.catch((err) => {
-      console.log(err);
-
-      if (err.code === 11000) {
-        return res.status(400).json({ error: 'Username is already in use.' });
-      }
-
-      return res.status(400).json({ error: 'An error occurred' });
-    });
-  });
+    res.json(csrfJSON);
 };
 
 module.exports.loginPage = loginPage;
 module.exports.login = login;
 module.exports.logout = logout;
-module.exports.signupPage = signupPage;
 module.exports.signup = signup;
+module.exports.getToken = getToken;
